b25fdea6d6faacdcf15c1164e4cf617277b70dfa
yashmarkad <yashmarkad41510@gmail.com>
2025-01-27 20:09:38 +0530

    Base-model-updated

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -9,6 +9,7 @@ from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
+
     def get_base_model(self):
         self.model = tf.keras.applications.mobilenet_v2.MobileNetV2(
             input_shape=self.config.params_image_size,
@@ -16,48 +17,58 @@ class PrepareBaseModel:
             include_top=self.config.params_include_top
         )
 
+        # Set the base model to non-trainable
+        self.model.trainable = False
+
         self.save_model(path=self.config.base_model_path, model=self.model)
+
     @staticmethod
-    def _prepare_full_model(model, classes):
-    # Add Global Average Pooling layer
-        gap_layer = tf.keras.layers.GlobalAveragePooling2D()(model.output)
-    
-    # Add the prediction layer
+    def _prepare_full_model(model, classes, freeze_all, freeze_till):
+        # Freeze layers as per the configuration
+        if freeze_all:
+            for layer in model.layers:
+                layer.trainable = False
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                layer.trainable = False
+
+        # Add a GlobalMaxPooling2D layer after the base model output
+        global_max_pool = tf.keras.layers.GlobalMaxPooling2D()(model.output)
+
+        # Add the prediction layer
         prediction = tf.keras.layers.Dense(
-        units=classes,
-        activation="softmax"
-        )(gap_layer)
-    
-    # Create the full model
+            units=classes,
+            activation="softmax"
+        )(global_max_pool)
+
+        # Define the full model
         full_model = tf.keras.models.Model(
-        inputs=model.input,
-        outputs=prediction
-    )
-    
-    # Compile the full model
+            inputs=model.input,
+            outputs=prediction
+        )
+
+        # Compile the model
         full_model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
-        loss=tf.keras.losses.CategoricalCrossentropy(),
-        metrics=["accuracy"]
-    )
-    
-    # Display the model summary
+            optimizer=tf.keras.optimizers.Adam(),
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
         full_model.summary()
         return full_model
 
-    
-    
     def update_base_model(self):
         self.full_model = self._prepare_full_model(
             model=self.model,
             classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
         )
 
         self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
 
-    
-        
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
         model.save(path)
+
 
